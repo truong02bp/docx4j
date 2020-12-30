435f687bf85de43b2f9e626d6af0b6c7394ab8cd
Truong <truong02bp>
2020-12-31 03:14:56 +0700

    update merge

--- src/main/java/com/demo/HomeController.java
+++ src/main/java/com/demo/HomeController.java
@@ -1,18 +1,16 @@
 package com.demo;
 
-import com.demo.entities.File;
 import com.demo.entities.Person;
 import com.demo.repository.FileRepository;
 import com.demo.service.impl.DocxService;
-import com.spire.doc.Document;
+//import com.spire.doc.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,32 +29,32 @@ public class HomeController {
     private static Map<Integer, String> map = new HashMap<>();
     private static int id = 0;
 
-    @GetMapping("/trang-chu")
-    public ResponseEntity<byte[]> homePage() throws Exception {
-        Document document = new Document();
-        document.loadFromFile("/home/truong02_bp/Desktop/template.docx");
-        String[] fields = document.getMailMerge().getMergeFieldNames();
-        Map<String, String> values = new HashMap<>();
-        Person person = new Person("Trường", "Hà Đông", "Siten", "0964279710");
-        String[] items = person.toString().split(";");
-        for (String item : items) {
-            String[] data = item.split("=");
-            values.put(data[0], data[1]);
-        }
-        String[] value = new String[fields.length + 1];
-        for (int i = 0; i < fields.length; i++)
-            value[i] = values.get(fields[i]);
-        document.getMailMerge().execute(fields, value);
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-//        document.saveToStream(os,FileFormat.Docx);
-        byte[] bytes = os.toByteArray();
-        String name = "result";
-        String type = "docx";
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.document"))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + name + "." + type)
-                .body(bytes);
-    }
+//    @GetMapping("/trang-chu")
+//    public ResponseEntity<byte[]> homePage() throws Exception {
+//        Document document = new Document();
+//        document.loadFromFile("/home/truong02_bp/Desktop/template.docx");
+//        String[] fields = document.getMailMerge().getMergeFieldNames();
+//        Map<String, String> values = new HashMap<>();
+//        Person person = new Person("Trường", "Hà Đông", "Siten", "0964279710");
+//        String[] items = person.toString().split(";");
+//        for (String item : items) {
+//            String[] data = item.split("=");
+//            values.put(data[0], data[1]);
+//        }
+//        String[] value = new String[fields.length + 1];
+//        for (int i = 0; i < fields.length; i++)
+//            value[i] = values.get(fields[i]);
+//        document.getMailMerge().execute(fields, value);
+//        ByteArrayOutputStream os = new ByteArrayOutputStream();
+////        document.saveToStream(os,FileFormat.Docx);
+//        byte[] bytes = os.toByteArray();
+//        String name = "result";
+//        String type = "docx";
+//        return ResponseEntity.ok()
+//                .contentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.document"))
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + name + "." + type)
+//                .body(bytes);
+//    }
 
 
     @GetMapping("/docx")
@@ -72,15 +70,18 @@ public class HomeController {
     }
 
     @GetMapping("/ckeditor")
-    public ModelAndView ckeditor() {
-        ModelAndView mav = new ModelAndView("ckeditor");
-        return mav;
+    public ResponseEntity<byte[]> ckeditor() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+        String filename = "pdf1.pdf";
+        headers.add("content-disposition", "inline;filename=" + filename);
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+        ResponseEntity<byte[]> result = new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
+        return result;
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<Integer> toDocx(@RequestBody Content content) {
-        id++;
-        map.put(id, content.getContent());
-        return ResponseEntity.ok(id);
+    @GetMapping("/merge")
+    public void merge(){
+        docxService.fillMailMerge();
     }
 }
--- src/main/java/com/demo/service/impl/DocxService.java
+++ src/main/java/com/demo/service/impl/DocxService.java
@@ -2,6 +2,8 @@ package com.demo.service.impl;
 
 
 import com.google.common.base.Preconditions;
+import com.spire.pdf.FileFormat;
+import com.spire.pdf.PdfDocument;
 import org.docx4j.TextUtils;
 import org.docx4j.TraversalUtil;
 import org.docx4j.XmlUtils;
@@ -24,12 +26,212 @@ import org.springframework.stereotype.Service;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.logging.Logger;
 
 @Service
 public class DocxService {
+    public ByteArrayOutputStream addHtmlToDocx(String content) throws Exception {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        FileInputStream is = null;
+        try {
+            is = new FileInputStream("/home/truong02_bp/Desktop/1.ADD-CK-GHDKX-1NG-KCC.docx");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        WordprocessingMLPackage document = null;
+        try {
+            document = WordprocessingMLPackage.load(is);
+        } catch (Docx4JException e) {
+            e.printStackTrace();
+        }
+        if (document != null) {
+            String html = "<html><body>" + content + "</body></html>";
+            XHTMLImporterImpl importer = new XHTMLImporterImpl(document);
+            // look for all P elements in the specified object
+            final List<P> paragraphs = new ArrayList<>();
+            MainDocumentPart mainPart = document.getMainDocumentPart();
+            new TraversalUtil(mainPart, new TraversalUtil.CallbackImpl() {
+                @Override
+                public List<Object> apply(Object o) {
+                    if (o instanceof P) {
+                        paragraphs.add((P) o);
+                    }
+                    return null;
+                }
+            });
+            for (final P paragraph : paragraphs) {
+                final StringWriter paragraphText = new StringWriter();
+                try {
+                    TextUtils.extractText(paragraph, paragraphText);
+                } catch (Exception ex) {
+                    System.out.println("Toang");
+                }
+                final String identifier = paragraphText.toString();
+                if (identifier.equals("abc")) {
+                    List<Object> listToModify;
+                    if (paragraph.getParent() instanceof Tc) {
+                        // paragraph located in table-cell
+                        final Tc parent = (Tc) paragraph.getParent();
+                        listToModify = parent.getContent();
+                    } else {
+                        // paragraph located in main document part
+                        listToModify = mainPart.getContent();
+                    }
+                    if (listToModify != null) {
+                        int index = listToModify.indexOf(paragraph);
+                        // remove the paragraph
+                        listToModify.remove(index);
+                        // add html
+                        listToModify.addAll(index, importer.convert(html, null));
+                    }
+                }
+            }
+            document.save(result);
+        }
+        return result;
+    }
 
+    public byte[] test(){
+        File file = new File("C:\\Users\\truon\\Downloads\\test.pdf");
+        Properties props = System.getProperties();
+        props.setProperty("javax.accessibility.assistive_technologies", "");
+        PdfDocument pdfDocument = new PdfDocument();
+        pdfDocument.loadFromFile(String.valueOf(file),FileFormat.PDF);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        pdfDocument.saveToStream(outputStream, FileFormat.PDF);
+        return outputStream.toByteArray();
+    }
+
+    public void fillMailMerge() {
+        FileInputStream is = null;
+        try {
+            is = new FileInputStream("C:\\Users\\truon\\Desktop\\1.ADD-CK-GHDKX-1NG-KCC.docx");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        WordprocessingMLPackage document = null;
+        try {
+            document = WordprocessingMLPackage.load(is);
+        } catch (Docx4JException e) {
+            e.printStackTrace();
+        }
+        if (document != null) {
+            List<String> mailMerges = getAllMailMerge(document.getMainDocumentPart().getContent());
+            List<Object> list = null;
+            try {
+                list = document.getMainDocumentPart().getJAXBNodesViaXPath("//w:checkBox",
+                        true);
+            } catch (JAXBException e) {
+                e.printStackTrace();
+            } catch (XPathBinderAssociationIsPartialException e) {
+                e.printStackTrace();
+            }
+            for (Object o : list) {
+                o = XmlUtils.unwrap(o);
+                CTFFCheckBox checkBox = (CTFFCheckBox) o;
+                BooleanDefaultTrue booleanDefaultTrue = new BooleanDefaultTrue();
+                booleanDefaultTrue.setVal(true);
+                checkBox.setChecked(booleanDefaultTrue);
+                CTFFData data = (CTFFData) checkBox.getParent();
+                CTFFName name = (CTFFName) data.getNameOrEnabledOrCalcOnExit().get(0).getValue();
+            }
+            Map<DataFieldName, String> values = new HashMap<>();
+            Random random = new Random();
+            String[] content = {"TỪ NAY DUYÊN KIẾP", "BỎ LẠI PHÍA SAU", "NGÀY VÀ BÓNG TỐI", "CHẲNG CÒN KHÁC NHAU"
+                , "CHẲNG CÓ NƠI NÀO YÊN BÌNH", "ĐƯỢC NHƯ EM BÊN ANH"};
+            for (String mailMerge : mailMerges) {
+                String value = content[random.nextInt(4)];
+                if (mailMerge.equals("Biển_kiểm_soát"))
+                    values.put(new DataFieldName(mailMerge), "\r\nTruc\r\n" +
+                            "A\r\n" +
+                            "B");
+                else
+                    values.put(new DataFieldName(mailMerge), value);
+            }
+            MailMerger.setMERGEFIELDInOutput(MailMerger.OutputField.KEEP_MERGEFIELD);
+            try {
+                MailMerger.performMerge(document, values, false);
+            } catch (Docx4JException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                document.save(new File("C:\\Users\\truon\\Desktop\\result.docx"));
+            } catch (Docx4JException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<String> getAllMailMerge(List<Object> objects) {
+        List<String> fields = new ArrayList<>();
+        for (Object o : objects) {
+            if (o instanceof JAXBElement) {
+                fields.addAll(getMailMergeFromTable(o));
+            } else
+                fields.addAll(getMailMerge(o.toString()));
+        }
+        return fields;
+    }
+
+    public List<String> getMailMergeFromTable(Object o) {
+        List<String> fields = new ArrayList<>();
+        o = ((JAXBElement<?>) o).getValue();
+        List<Object> texts = null;
+        if (o.getClass().equals(Tbl.class)) {
+            Tbl table = (Tbl) o;
+            texts = getAllElementFromObject(table, Text.class);
+        } else if (o.getClass().equals(CTBookmark.class)) {
+            CTBookmark ctBookmark = (CTBookmark) o;
+            texts = getAllElementFromObject(ctBookmark.getParent(), Text.class);
+        }
+        if (texts != null) {
+            StringBuilder stringBuilder = new StringBuilder("");
+            for (Object t :
+                    texts) {
+                Text tx = (Text) t;
+                stringBuilder.append(tx.getValue());
+            }
+            fields.addAll(getMailMerge(stringBuilder.toString()));
+        }
+        return fields;
+    }
+
+    public List<String> getMailMerge(String content) {
+        List<String> fields = new ArrayList<>();
+        if ((content.contains("MERGEFIELD") && content.contains("«") && content.contains("»"))) {
+            StringTokenizer stringTokenizer = new StringTokenizer(content, " ");
+            while (stringTokenizer.hasMoreTokens()) {
+                String value = stringTokenizer.nextToken();
+                if (value.equals("MERGEFIELD")) {
+                    String nextToken = stringTokenizer.nextToken();
+                    nextToken = nextToken.substring(0,nextToken.lastIndexOf("«"));
+                    if (nextToken.contains("\""))
+                        nextToken = nextToken.substring(1, nextToken.length() - 1);
+                    System.out.println(nextToken);
+                    fields.add(nextToken);
+                }
+            }
+        }
+        return fields;
+    }
+
+    private static List<Object> getAllElementFromObject(Object obj, Class<?> toSearch) {
+        List<Object> result = new ArrayList<>();
+        if (obj instanceof JAXBElement) obj = ((JAXBElement<?>) obj).getValue();
+
+        if (obj.getClass().equals(toSearch))
+            result.add(obj);
+        else if (obj instanceof ContentAccessor) {
+            List<?> children = ((ContentAccessor) obj).getContent();
+            for (Object child : children) {
+                result.addAll(getAllElementFromObject(child, toSearch));
+            }
+        }
+        return result;
+    }
 //    public ByteArrayOutputStream exportDocx(String content){
 ////        String outPath = "/home/truong02_bp/Desktop/value.docx";
 //        ByteArrayInputStream is = new ByteArrayInputStream(content.getBytes());
@@ -110,190 +312,6 @@ public class DocxService {
 //            }
 //        }
 //    }
-
-    public ByteArrayOutputStream addHtmlToDocx(String content) throws Exception {
-        ByteArrayOutputStream result = new ByteArrayOutputStream();
-        FileInputStream is = null;
-        try {
-            is = new FileInputStream("/home/truong02_bp/Desktop/1.ADD-CK-GHDKX-1NG-KCC.docx");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        WordprocessingMLPackage document = null;
-        try {
-            document = WordprocessingMLPackage.load(is);
-        } catch (Docx4JException e) {
-            e.printStackTrace();
-        }
-        if (document != null) {
-            String html = "<html><body>" + content + "</body></html>";
-            XHTMLImporterImpl importer = new XHTMLImporterImpl(document);
-            // look for all P elements in the specified object
-            final List<P> paragraphs = new ArrayList<>();
-            MainDocumentPart mainPart = document.getMainDocumentPart();
-            new TraversalUtil(mainPart, new TraversalUtil.CallbackImpl() {
-                @Override
-                public List<Object> apply(Object o) {
-                    if (o instanceof P) {
-                        paragraphs.add((P) o);
-                    }
-                    return null;
-                }
-            });
-            for (final P paragraph : paragraphs) {
-                final StringWriter paragraphText = new StringWriter();
-                try {
-                    TextUtils.extractText(paragraph, paragraphText);
-                } catch (Exception ex) {
-                    System.out.println("Toang");
-                }
-                final String identifier = paragraphText.toString();
-                if (identifier.equals("abc")) {
-                    List<Object> listToModify;
-                    if (paragraph.getParent() instanceof Tc) {
-                        // paragraph located in table-cell
-                        final Tc parent = (Tc) paragraph.getParent();
-                        listToModify = parent.getContent();
-                    } else {
-                        // paragraph located in main document part
-                        listToModify = mainPart.getContent();
-                    }
-                    if (listToModify != null) {
-                        int index = listToModify.indexOf(paragraph);
-                        // remove the paragraph
-                        listToModify.remove(index);
-                        // add html
-                        listToModify.addAll(index, importer.convert(html, null));
-                    }
-                }
-            }
-            document.save(result);
-        }
-        return result;
-    }
-
-    public void readMailMerge() {
-        FileInputStream is = null;
-        try {
-            is = new FileInputStream("/home/truong02_bp/Downloads/1.ADD CK GH,DKX 1NG KCC.docx");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        WordprocessingMLPackage document = null;
-        try {
-            document = WordprocessingMLPackage.load(is);
-        } catch (Docx4JException e) {
-            e.printStackTrace();
-        }
-        if (document != null) {
-            List<String> mailMerges = getAllMailMerge(document.getMainDocumentPart().getContent());
-            List<Object> list = null;
-            try {
-                list = document.getMainDocumentPart().getJAXBNodesViaXPath("//w:checkBox",
-                        true);
-            } catch (JAXBException e) {
-                e.printStackTrace();
-            } catch (XPathBinderAssociationIsPartialException e) {
-                e.printStackTrace();
-            }
-            for (Object o : list) {
-                o = XmlUtils.unwrap(o);
-                CTFFCheckBox checkBox = (CTFFCheckBox) o;
-                BooleanDefaultTrue booleanDefaultTrue = new BooleanDefaultTrue();
-                booleanDefaultTrue.setVal(true);
-                checkBox.setChecked(booleanDefaultTrue);
-                CTFFData data = (CTFFData) checkBox.getParent();
-                CTFFName name = (CTFFName) data.getNameOrEnabledOrCalcOnExit().get(0).getValue();
-                System.out.println(name.getVal());
-            }
-            String[] content = {"Trường", "Chường", "Hello", "Goodbye"};
-            Map<DataFieldName, String> values = new HashMap<>();
-            Random random = new Random();
-            for (String mailMerge : mailMerges) {
-                String value = content[random.nextInt(4)];
-                values.put(new DataFieldName(mailMerge), value);
-            }
-            MailMerger.setMERGEFIELDInOutput(MailMerger.OutputField.REMOVED);
-            try {
-                MailMerger.performMerge(document, values, false);
-            } catch (Docx4JException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                document.save(new File("/home/truong02_bp/Desktop/result.docx"));
-            } catch (Docx4JException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public List<String> getAllMailMerge(List<Object> objects) {
-        List<String> fields = new ArrayList<>();
-        for (Object o : objects) {
-            if (o instanceof JAXBElement) {
-                fields.addAll(getMailMergeFromTable(o));
-            } else
-                fields.addAll(getMailMerge(o.toString()));
-        }
-        return fields;
-    }
-
-    public List<String> getMailMergeFromTable(Object o) {
-        List<String> fields = new ArrayList<>();
-        o = ((JAXBElement<?>) o).getValue();
-        List<Object> texts = null;
-        if (o.getClass().equals(Tbl.class)) {
-            Tbl table = (Tbl) o;
-            texts = getAllElementFromObject(table, Text.class);
-        } else if (o.getClass().equals(CTBookmark.class)) {
-            CTBookmark ctBookmark = (CTBookmark) o;
-            texts = getAllElementFromObject(ctBookmark.getParent(), Text.class);
-        }
-        if (texts != null) {
-            StringBuilder stringBuilder = new StringBuilder("");
-            for (Object t :
-                    texts) {
-                Text tx = (Text) t;
-                stringBuilder.append(tx.getValue());
-            }
-            fields.addAll(getMailMerge(stringBuilder.toString()));
-        }
-        return fields;
-    }
-
-    public List<String> getMailMerge(String content) {
-        List<String> fields = new ArrayList<>();
-        if ((content.contains("MERGEFIELD") && content.contains("«") && content.contains("»"))) {
-            StringTokenizer stringTokenizer = new StringTokenizer(content, " ");
-            while (stringTokenizer.hasMoreTokens()) {
-                String value = stringTokenizer.nextToken();
-                if (value.equals("MERGEFIELD")) {
-                    String nextToken = stringTokenizer.nextToken();
-                    if (nextToken.contains("\""))
-                        nextToken = nextToken.substring(1, nextToken.length() - 1);
-                    fields.add(nextToken);
-                }
-            }
-        }
-        return fields;
-    }
-
-    private static List<Object> getAllElementFromObject(Object obj, Class<?> toSearch) {
-        List<Object> result = new ArrayList<>();
-        if (obj instanceof JAXBElement) obj = ((JAXBElement<?>) obj).getValue();
-
-        if (obj.getClass().equals(toSearch))
-            result.add(obj);
-        else if (obj instanceof ContentAccessor) {
-            List<?> children = ((ContentAccessor) obj).getContent();
-            for (Object child : children) {
-                result.addAll(getAllElementFromObject(child, toSearch));
-            }
-        }
-        return result;
-    }
-
 
 //    public void docxToHtml() throws Exception {
 //        File file = new File("/home/truong02_bp/Downloads/1.ADD CK GH,DKX 1NG KCC.docx");
